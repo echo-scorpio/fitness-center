6c854263105850e1de0b11797815a70a653ed69c
scoopcc <353688404@qq.com>
2020-03-04 17:40:58 +0800

    修改数据库配置文件

--- config.inc.php
+++ config.inc.php
@@ -2,7 +2,7 @@
 return array(
 	'DB_TYPE'			=>	'mysql',
 	'DB_HOST'			=>	'127.0.0.1',
-	'DB_NAME'			=>	'recomend',//��Ҫ�½�һ�����ݿ⣡���ֽ�
+	'DB_NAME'			=>	'test',//��Ҫ�½�һ�����ݿ⣡���ֽ�
 	'DB_USER'			=>	'root',		//���ݿ��û���	
 	'DB_PWD'			=>	'root',//���ݿ��¼����
 	'DB_PORT'			=>	'3306',
