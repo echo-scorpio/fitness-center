eee055fa8f13f6428b18dfcd43aab582223a0a9b
scoopcc <353688404@qq.com>
2020-03-04 17:10:48 +0800

    添加配置文;文件

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	
+	define('THINK_PATH','./ThinkPHP/');
+	define('APP_PATH','./home/');
+	define('APP_NAME','home');
+	define('STRIP_RUNTIME_SPACE',false);
+	define('APP_DEBUG',true);
+	define('__PUBLIC__','__ROOT__/public');
+
+	require		THINK_PATH.'ThinkPHP.php';
+
+?>
